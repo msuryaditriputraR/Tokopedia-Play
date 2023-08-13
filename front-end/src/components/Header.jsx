452d5c436c0affa10ee281247ab09a4ae27c9c45
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 13:17:12 +0800

    add: header

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import Search from "./Search";
+
+const Header = () => {
+  return (
+    <header className="fixed left-0 top-0 z-50 flex h-16 w-full items-center justify-between rounded-b-2xl bg-green-200 bg-opacity-5 bg-clip-padding px-14 shadow backdrop-blur-xl backdrop-filter">
+      <img src="./logo.png" alt="logo" className="w-[150px] drop-shadow-md" />
+      <Search />
+      <Button text={"Sign In"} handleClick={() => {}} />
+    </header>
+  );
+};
+
+export default Header;
