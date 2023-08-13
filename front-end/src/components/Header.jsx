3be86cbe462f05cc304a9f99fc45795b2f311999
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 20:49:20 +0800

    refactoring button in header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     <header className="fixed left-0 top-0 z-50 flex h-16 w-full items-center justify-between rounded-b-2xl bg-green-200 bg-opacity-5 bg-clip-padding px-14 shadow backdrop-blur-xl backdrop-filter">
       <img src="/logo.png" alt="logo" className="w-[150px] drop-shadow-md" />
       <Search />
-      <Button text={"Sign In"} handleClick={() => {}} />
+      <Button handleClick={() => {}} cls="px-4 py-2 rounded-md">
+        Sign In
+      </Button>
     </header>
   );
 };
