4e91dd2b484ec4af26d3fe2c60ef4e46652ba499
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 13:16:17 +0800

    add: font poppins

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
