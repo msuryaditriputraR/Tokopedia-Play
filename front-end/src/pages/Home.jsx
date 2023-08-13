28e7b3ceff98fcfca80337a2a01e717b556f2803
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 16:21:28 +0800

    wrap tab provider

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,13 +2,16 @@ import Header from "../components/Header";
 import { Panel } from "../components/Panel";
 import Tabs from "../components/tabs";
 import Footer from "../components/Footer";
+import { TabsProvider } from "../context/TabsContext";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Tabs />
-      <Panel />
+      <TabsProvider>
+        <Tabs />
+        <Panel />
+      </TabsProvider>
       <Footer />
     </>
   );
