7a2c5cf005837f3fe28dbe88c177aeb97ad75f4f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 21:46:47 +0800

    fix: scroll comments

--- front-end/src/components/Comments.jsx
+++ front-end/src/components/Comments.jsx
@@ -10,23 +10,33 @@ const Comments = () => {
       time: new Date("08/12/2023"),
     },
     {
-      username: "dandi",
+      username: "dandii",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, nihil!",
       time: Date.now(),
+    },
+    {
+      username: "bondannnnnnnnnnnnnn",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, nihil!",
+      time: new Date("08/09/2023"),
+    },
+    {
+      username: "username",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sint esse nulla commodi nihil asperiores, quod, necessitatibus quam sequi eius aspernatur impedit a totam eveniet fuga itaque repellat dignissimos minima. Consequatur error eius repudiandae amet dolor laborum blanditiis vel, quia ut maxime, cumque, suscipit asperiores omnis alias commodi a totam!",
+      time: new Date("08/09/2023"),
     },
   ];
 
   return (
-    <div className="relative overflow-hidden rounded-3xl border border-slate-200 shadow hover:shadow-md">
-      <h2 className="absolute top-0 w-full border border-slate-200 px-4 py-3 font-semibold">
+    <div className="relative max-h-[430px] overflow-hidden rounded-3xl border border-slate-200 shadow hover:shadow-md">
+      <h2 className="absolute top-0 z-10 w-full border border-slate-200 bg-white px-4 py-3 font-semibold">
         Comments
       </h2>
-      <div className="flex flex-col gap-y-2">
+      <div className="no-scrollbar flex max-h-full flex-col gap-y-2 overflow-y-auto">
         {data.map((d, i) => (
           <CommentCard key={i} comment={d} />
         ))}
       </div>
-      <form className="absolute bottom-4 left-[50%] flex w-11/12 translate-x-[-50%] items-center gap-x-3 rounded-full border border-slate-200 px-4 py-2 shadow">
+      <form className="absolute bottom-4 left-[50%] z-10 flex w-11/12 translate-x-[-50%] items-center gap-x-3 rounded-full border border-slate-200 bg-white px-4 py-2 shadow">
         <input
           type="text"
           className="flex-1 bg-transparent py-1 text-sm caret-green-500 outline-none"
--- front-end/src/components/cards/CommentCard.jsx
+++ front-end/src/components/cards/CommentCard.jsx
@@ -3,9 +3,9 @@ import { formatRelative } from "date-fns";
 const CommentCard = ({ comment }) => {
   const { username, time, text } = comment;
   return (
-    <article className="mx-auto w-11/12 rounded-lg border border-slate-200 bg-white p-2 shadow-sm first:mt-14">
+    <article className="mx-auto w-11/12 rounded-lg border border-slate-200 bg-white p-2 shadow-sm first:mt-14 last:mb-20">
       <div className="mb-1 flex items-center justify-between">
-        <h2 className="font-semibold">{username}</h2>
+        <h2 className="flex-1 truncate font-semibold">{username}</h2>
         <span className="text-xs text-slate-400">
           {formatRelative(time, new Date())}
         </span>
