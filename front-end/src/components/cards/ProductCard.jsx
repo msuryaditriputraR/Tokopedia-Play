acfd43b2f765d61b6bb079745a2ed02d40f23b76
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 20:14:41 +0800

    add: productcard component

---
+++ front-end/src/components/cards/ProductCard.jsx
@@ -0,0 +1,22 @@
+const ProductCard = ({ product }) => {
+  const { title, price, imageURL, linkProduct } = product;
+  return (
+    <article className="w-[165px] rounded-xl border border-slate-200 bg-white p-3 shadow-sm transition-all hover:scale-105 hover:shadow-lg">
+      <a href={linkProduct} target="_blank" rel="noopener noreferrer">
+        <div className="relative mb-4 overflow-hidden">
+          <img
+            src={imageURL}
+            alt={title}
+            className="h-[150px] w-full rounded-md"
+          />
+        </div>
+        <div className="px-1">
+          <h3 className="line-clamp-2 font-semibold">{title}</h3>
+          <p className="font-semibold text-green-500">{price}</p>
+        </div>
+      </a>
+    </article>
+  );
+};
+
+export default ProductCard;
