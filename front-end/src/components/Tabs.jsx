c90a61a27013fbf479c63db0fecf3f485f544ea2
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 16:21:38 +0800

    change active tab

--- front-end/src/components/Tabs.jsx
+++ front-end/src/components/Tabs.jsx
@@ -1,12 +1,23 @@
+import { useContext } from "react";
+import { TabsContext } from "../context/TabsContext";
+
 const Tabs = () => {
   const tabsMenu = ["Explore", "Fashion", "Elektronik", "Health & Care"];
+
+  const { activeTab, setActiveTab } = useContext(TabsContext);
+
   return (
     <section className="mt-20">
       <ul className="flex gap-x-2">
         {tabsMenu.map((menu, index) => (
           <li
             key={index}
-            className=" cursor-pointer  rounded-full border border-slate-300 px-4 py-2 capitalize shadow-sm transition hover:bg-green-500  hover:text-white hover:shadow-md"
+            className={`cursor-pointer  rounded-full border border-slate-300 px-4 py-2 capitalize shadow-sm transition hover:bg-green-500  hover:text-white hover:shadow-md ${
+              activeTab === menu
+                ? "bg-green-500 text-white"
+                : "bg-white text-current"
+            }`}
+            onClick={() => setActiveTab(menu)}
           >
             {menu}
           </li>
