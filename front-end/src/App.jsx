aa90994d6e0a1f0acb95521fb34170342d65a474
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 13:17:22 +0800

    add: header to app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-    return (
-        <h1 className="text-3xl font-bold underline text-red-300">
-            Hello world!
-        </h1>
-    );
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
