3d5a06d4e623b25cadef793761b0a7d8ba398bbf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 16:23:32 +0800

    get active tab from local storage

--- front-end/src/context/TabsContext.jsx
+++ front-end/src/context/TabsContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const TabsContext = createContext();
 
 export const TabsProvider = ({ children }) => {
-  const [activeTab, setActiveTab] = useState("Explore");
+  const [activeTab, setActiveTab] = useState(
+    localStorage.getItem("activeTab") || "Explore",
+  );
 
   return (
     <TabsContext.Provider value={{ activeTab, setActiveTab }}>
