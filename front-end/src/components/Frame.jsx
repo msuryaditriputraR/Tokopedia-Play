cf54f9cd1875f267f457852ab397c72adabc1877
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 17:53:11 +0800

    add: frame Component

---
+++ front-end/src/components/Frame.jsx
@@ -0,0 +1,14 @@
+const Frame = ({ title, linkVideo, videoId }) => {
+  return (
+    <div className="relative col-span-2 row-span-2 h-0 pb-[56.25%]">
+      <iframe
+        className="absolute left-0 top-0 h-full w-full rounded-2xl shadow-md"
+        src={`${linkVideo}?controls=0&autoplay=1&loop=1&rel=0&playlist=${videoId}`}
+        title={title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Frame;
