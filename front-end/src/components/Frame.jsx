1db6142cbfc7df4de3fb02ae8ade06a2bf0a755f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-13 20:49:08 +0800

    remove space between class

--- front-end/src/components/Frame.jsx
+++ front-end/src/components/Frame.jsx
@@ -1,6 +1,6 @@
 const Frame = ({ title, linkVideo, videoId }) => {
   return (
-    <div className="relative col-span-2  h-0 pb-[56.25%]">
+    <div className="relative col-span-2 h-0 pb-[56.25%]">
       <iframe
         className="absolute left-0 top-0 h-full w-full rounded-2xl shadow-md"
         src={`${linkVideo}?controls=0&autoplay=1&loop=1&rel=0&playlist=${videoId}`}
