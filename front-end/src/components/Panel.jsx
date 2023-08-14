f37780979c86116625d4dc26e6086eb95a731f09
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-15 00:52:40 +0800

    update list video

--- front-end/src/components/Panel.jsx
+++ front-end/src/components/Panel.jsx
@@ -1,26 +1,24 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { TabsContext } from "../context/TabsContext";
 import VideoCard from "./cards/videoCard";
 import useFetch from "../hooks/useFetch";
 
 export const Panel = () => {
-  useEffect(() => {
-    fetch(import.meta.env.VITE_ROOT_API + "videos").then((response) =>
-      console.log(response),
-    );
-  });
+  const [data] = useFetch(import.meta.env.VITE_ROOT_API + "videos");
 
   const { activeTab } = useContext(TabsContext);
 
   return (
-    <section className="mt-5">
-      {/* <div className="grid grid-cols-3 items-start gap-6">
-        {activeTab === "Explore"
-          ? data.map((d, i) => <VideoCard key={i} video={d} index={i} />)
-          : data
-              .filter((d) => d.category.includes(activeTab))
-              .map((d, i) => <VideoCard key={i} video={d} index={i} />)}
-      </div> */}
-    </section>
+    data && (
+      <section className="mt-5">
+        <div className="grid grid-cols-3 items-start gap-6">
+          {activeTab === "Explore"
+            ? data.map((d, i) => <VideoCard key={i} video={d} index={i} />)
+            : data
+                .filter((d) => d.category.includes(activeTab))
+                .map((d, i) => <VideoCard key={i} video={d} index={i} />)}
+        </div>
+      </section>
+    )
   );
 };
