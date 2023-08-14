58ff58c307533f425b031627c9fb6e192ffb0834
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-14 14:05:18 +0800

    remove statis images

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,3 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
 import express from 'express';
 import 'dotenv/config';
 import mongoConnect from './config/mongo.js';
@@ -9,13 +7,9 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const imgDir = path.join(__dirname, 'images');
-
 // MIDDLEWARE
 app.use(express.json());
 app.use(router);
-app.use('/images', express.static(imgDir));
 
 // CONNECT MONGODB
 mongoConnect();
