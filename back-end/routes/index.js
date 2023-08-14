6ba5432333a7a28ab69cab2028b8c31fc8f1d4e4
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-15 00:09:48 +0800

    add: comment each route

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -5,10 +5,15 @@ import videoController from '../controllers/video.js';
 import productController from '../controllers/product.js';
 import commentController from '../controllers/comment.js';
 
+// VIDEOS
 router.get('/videos', videoController.getVideoList);
 router.post('/videos', videoController.postVideo);
+
+// PRODUCTS
 router.get('/video/:videoId/products', productController.getProductList);
 router.post('/video/:videoId/products', productController.postProduct);
+
+// COMMENTS
 router.get('/video/:videoId/comments', commentController.getCommentList);
 router.post('/video/:videoId/comments', commentController.postComment);
 
