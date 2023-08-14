6016b36cc7ac8723ec724d8ecf829bb75b98a1e2
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-15 00:42:13 +0800

    add: use Cors middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import router from './routes/index.js';
 
@@ -8,6 +9,7 @@ const app = express();
 
 // MIDDLEWARE
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1/', router);
 
 // START SERVER
