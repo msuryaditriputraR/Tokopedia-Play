3887044ed62f57efb9598f72a2dd65e97c5665fc
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-14 22:09:37 +0800

    delete mongoose

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'dotenv/config';
-import mongoConnect from './config/mongo.js';
 import router from './routes/index.js';
 
 const port = process.env.PORT || 3000;
@@ -10,9 +9,6 @@ const app = express();
 // MIDDLEWARE
 app.use(express.json());
 app.use(router);
-
-// CONNECT MONGODB
-mongoConnect();
 
 // START SERVER
 app.listen(port, () => {
