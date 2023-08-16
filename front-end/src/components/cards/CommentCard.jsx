5f28a86b4d2949350b210fcf5b4c4fa83b67fcb6
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-16 21:33:00 +0800

    add: scrollintoview

--- front-end/src/components/cards/CommentCard.jsx
+++ front-end/src/components/cards/CommentCard.jsx
@@ -1,10 +1,17 @@
 import { formatRelative } from "date-fns";
+import { useEffect } from "react";
 
 const CommentCard = ({ comment }) => {
   const { username, createdAt, message } = comment;
 
+  useEffect(() => {
+    document
+      .querySelector(".comment-card:last-child")
+      .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  }, []);
+
   return (
-    <article className="mx-auto w-11/12 rounded-lg border border-slate-200 bg-white p-2 shadow-sm first:mt-14 last:mb-20">
+    <article className="comment-card mx-auto w-11/12 rounded-lg border border-slate-200 bg-white p-2 shadow-sm first:mt-14 ">
       <div className="mb-1 flex items-center justify-between">
         <h2 className="flex-1 truncate font-semibold">{username}</h2>
         <span className="text-xs text-slate-400">
